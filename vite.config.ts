56759927ebc049168aa4c629fece8f3ece3584a0
chris903224 <christianvillanuevac9@gmail.com>
2026-05-23 00:59:42 +0800

    fix: update vite config with all html files

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    port: 3000,
-    open: true
+    port: 3000
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: false
-  },
-  preview: {
-    port: 3000
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        admin: 'admin.html',
+        'admin-login': 'admin-login.html',
+        'admin-callback': 'admin-callback.html'
+      }
+    }
   }
 });
